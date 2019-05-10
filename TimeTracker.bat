ec9c8765866d75cae68f03567fadf295898b75c9
Swapnil Sonar <Sonar_S@India.XoriantCorp.com>
2019-05-10 16:54:24 +0530

    updated

--- TimeTracker.bat
+++ TimeTracker.bat
@@ -1,3 +1,5 @@
-java -cp TimeTracker.jar com.swapnil.timetracker.FeedTime
-java -cp TimeTracker.jar com.swapnil.timetracker.TimeTracker
+:: java -cp TimeTracker.jar com.swapnil.timetracker.FeedTime
+:: java -cp TimeTracker.jar com.swapnil.timetracker.TimeTracker
+java -cp ./target/TimeTracker-1.jar com.swapnil.timetracker.FeedAndTrackTime
 timetracker_out.html
+
